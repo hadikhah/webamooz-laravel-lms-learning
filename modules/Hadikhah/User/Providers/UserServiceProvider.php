2985d7c61760c60cf9b4e114717304f89ff42c30
Mohammad Ali Haidkhah <hadikhahe@gmail.com>
2021-04-08 12:27:39 +0430

    Hadikhah package and User module introdused in composer.json and config/app and connected to laravel

---
+++ modules/Hadikhah/User/Providers/UserServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hadikhah\User\Providers;
+
+use \Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+}
